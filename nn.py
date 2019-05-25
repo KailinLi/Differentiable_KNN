c278745d3a8c9738855f3984a4334ac08d0b1222
fluorinedog <fluorinedog@gmail.com>
2019-05-25 23:51:53 +0800

    udpate

--- nn.py
+++ nn.py
@@ -45,19 +45,19 @@ if __name__ == "__main__":
     criterion = nn.CrossEntropyLoss()
     optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 
-    for epoch in range(num_epoches):
-        for i, (images, labels) in enumerate(train_loader):
-            images = Variable(images.view(-1, 28 * 28))
-            labels = Variable(labels)
+    # for epoch in range(num_epoches):
+    #     for i, (images, labels) in enumerate(train_loader):
+    #         images = Variable(images.view(-1, 28 * 28))
+    #         labels = Variable(labels)
 
-            optimizer.zero_grad()
-            outputs = model(images)
-            loss = criterion(outputs, labels)
-            loss.backward()
-            optimizer.step()
+    #         optimizer.zero_grad()
+    #         outputs = model(images)
+    #         loss = criterion(outputs, labels)
+    #         loss.backward()
+    #         optimizer.step()
 
-            if i % 100 == 0:
-                print('current loss = %.5f' % loss.data)
+    #         if i % 100 == 0:
+    #             print('current loss = %.5f' % loss.data)
 
     total = 0
     correct = 0
@@ -70,4 +70,5 @@ if __name__ == "__main__":
         total += labels.size(0)
         correct += (predicts == labels).sum()
 
+    print(total)
     print('Accuracy = %.2f' % (100 * correct / total))
